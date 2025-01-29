8acb74af0189b9780a14caddc22d6aa5a3c85724
Felipe Martín <felipemartinrod@gmail.com>
2025-01-28 22:05:19 -0500

    front: Actualización DashboardLayout

--- front/src/layout/DashBoardLayout.tsx
+++ front/src/layout/DashBoardLayout.tsx
@@ -1,29 +1,21 @@
 import { Box, Grid2 } from "@mui/material";
 import { Outlet } from "react-router";
-import { Fragment } from "react/jsx-runtime";
 import Bar from "../components/Dashboard/Bar";
 import Menu from "../components/Dashboard/Menu";
 
 const DashBoardLayout = () => {
-  
   return (
-    <Fragment key="dashboard">
-      <Box sx={{ height: "100vh" }}>
-        <Bar />
-        <Grid2
-          sx={{ width: "100%", height: "100vh", padding: "16px" }}
-          container
-          spacing={2}
-        >
-          <Grid2 size={3}>
-            <Menu />
-          </Grid2>
-          <Grid2 size={9}>
-            <Outlet />
-          </Grid2>
+    <Box sx={{ height: "100vh" }}>
+      <Bar />
+      <Grid2 sx={{ width: "100%", padding: "16px" }} container spacing={2}>
+        <Grid2 size={3}>
+          <Menu />
         </Grid2>
-      </Box>
-    </Fragment>
+        <Grid2 size={9}>
+          <Outlet />
+        </Grid2>
+      </Grid2>
+    </Box>
   );
 };
 
